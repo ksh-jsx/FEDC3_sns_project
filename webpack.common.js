3244854793ef91ec295efaecb27e71e2631e6d05
김성현 <shkim061198@gmail.com>
2023-01-04 23:24:06 +0900

    Readme 작성

--- webpack.common.js
+++ webpack.common.js
@@ -19,22 +19,18 @@ module.exports = {
     clean: true,
   },
   plugins: [
-    //html파일에 JS 번들을 자동으로 묶어주는 플러그인
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, './public/index.html'),
       favicon: './static/images/logo.png',
     }),
-    //디렉토리를 카피하여 dist에 들어갈 수 있도록 설정하는 플러그인
     new CopyWebpackPlugin({
       patterns: [{ from: 'static' }],
     }),
-    //js안에서 호출되는 스타일 코드를 파일로 추출
     isProduction &&
       new MiniCSSExtractPlugin({
         filename: '[name].css',
       }),
-    //빌드되고 사용되지 않는 찌꺼기 파일들이나 캐시처럼 남아있는 파일들을 빌드 전에 삭제해주는 플러그인
     new CleanWebpackPlugin(),
   ].filter(Boolean),
   module: {
