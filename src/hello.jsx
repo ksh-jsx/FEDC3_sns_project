838321d8e39e57f30b451efb0069f321bac45b93
김성현 <shkim061198@gmail.com>
2023-01-09 16:02:44 +0900

    feat: hi추가

--- src/hello.jsx
+++ src/hello.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const hello = () => {
   return (
     <div>
-      zzzzzzzzzz
+      hellohellohellohello
     </div>
   );
 };
---
+++ src/hi.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const hi = () => {
+  return (
+    <div>
+      hihihihihihi
+    </div>
+  );
+};
+
+export default hi;
